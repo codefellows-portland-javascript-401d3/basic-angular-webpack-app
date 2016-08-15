e8ef7c9ba77cc4326259246123343ab817dc4900
Kate Sowles <katesowles@gmail.com>
2016-08-15 16:07:47 -0700

    basic requirements for app are complete

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import '../app/css/main.css';
+import angular from 'angular';
+
+const app = angular.module('kateGreeting', []);
+
+app.controller('main', function() { });
+
+//////////////////////////////////////////////////////////////
+
+function allFilled () {
+  // this function will check and see if all inputs are filled, if so the results will display, if not then no results
+}
+
+allFilled();
